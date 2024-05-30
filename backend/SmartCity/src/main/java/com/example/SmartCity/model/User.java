a2e807c973792c7051f66eb08ec92d091ccdbca6
Andra Ciobanu <ciobanuandramaria@gmail.com>
2024-05-30 18:34:40 +0300

    login+register done

---
+++ backend/SmartCity/src/main/java/com/example/SmartCity/model/User.java
@@ -0,0 +1,28 @@
+package com.example.SmartCity.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "users",schema="smartcity")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    @Email
+    private String username;
+    @NotBlank
+    private String password;
+    private String role;
+}
