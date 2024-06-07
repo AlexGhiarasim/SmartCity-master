54d78883a2f78a7c4f270b4f210201e35223b6d2
AlexGhiarasim <alexandru.ghiarasim03@gmail.com>
2024-06-07 22:34:12 +0300

    .

--- backend/SmartCity/src/main/java/com/example/SmartCity/SmartCityApplication.java
+++ backend/SmartCity/src/main/java/com/example/SmartCity/SmartCityApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmartCityApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SmartCityApplication.class, args);
 	}
 
 }
+
---
+++ frontend/public/js/LoginSignUpReqs.js
@@ -0,0 +1,110 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    let counterSignUp = 0;
+    let counterLogin = 1;
+
+    loginButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        counterLogin++;
+        counterSignUp = 0;
+        const nameInput = document.getElementById("nameInput");
+        const passwordInput = document.querySelector(".input input[type='password']");
+
+        if (counterLogin > 1) {
+            const action = document.getElementById("actionText").textContent;
+            if (action === "Login") {
+                const name = nameInput.value;
+                const password = passwordInput.value;
+
+                console.log(name +" "+ password);
+
+                if (name.trim() !== "" && password.trim() !== "") {
+
+                    const credentials = {
+                        username: name,
+                        password: password
+                    };
+
+                    fetch("/login", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json"
+                        }, body: JSON.stringify(credentials)
+                    }).then(response => {
+                        if (response.ok) {
+                            // cerere GET autentificata la pagina de home
+                        } else {
+                            // aici voi face o caseta pt raspuns incorect
+                            throw new Error("Username or password incorrect");
+                        }
+                    })
+                        .catch(error => {
+                            alert(error.message);
+                        });
+                } else {
+                    alert("Please enter both name and password");
+                }
+            }
+        }
+        nameInput.value = '';
+        passwordInput.value = '';
+    });
+
+    signUpButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        counterSignUp++;
+        counterLogin = 0;
+
+        const nameInput = document.getElementById("nameInput");
+        const passwordInput = document.querySelector(".input input[type='password']");
+        const confirmPasswordInput = document.querySelector("#confirmPasswordInput input[type='password']");
+
+        if (counterSignUp > 1) {
+            const action = document.getElementById("actionText").textContent;
+            if (action === "Sign Up") {
+
+                const name = nameInput.value;
+                const password = passwordInput.value;
+                const confirmPassword = confirmPasswordInput.value;
+
+                nameInput.value = '';
+                passwordInput.value = '';
+                confirmPasswordInput.value = '';
+
+                if (name.trim() !== "" && password.trim() !== "" && confirmPassword.trim() !== "") {
+                    if (password !== confirmPassword) {
+                        alert("Passwords do not match");
+                        return;
+                    }
+
+                    const newUser = {
+                        name: name,
+                        password: password
+                    };
+
+                    fetch("/signup", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify(newUser)
+                    }).then(response => {
+                        if (response.ok) {
+                            // cerere GET autentificata la pagina de home
+                        } else {
+                            throw new Error("Signup failed");
+                        }
+                    }).catch(error => {
+                        alert(error.message);
+                    });
+                } else {
+                    alert("Please enter both name and password");
+                }
+            }
+        }
+
+        nameInput.value = '';
+        passwordInput.value = '';
+        confirmPasswordInput.value = '';
+    });
+});
